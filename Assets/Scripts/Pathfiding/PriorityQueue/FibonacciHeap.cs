74baf30e811c568394f0dacfad59e01f6d1e682f
Luiz Ricardo Paiva Santiago <luiz.ricardo@gizmin.com>
2018-11-05 18:33:15 -0300

    chg - Inicio Implementação FibHeap.

--- Assets/Scripts/Pathfiding/PriorityQueue/FibonacciHeap.cs
+++ Assets/Scripts/Pathfiding/PriorityQueue/FibonacciHeap.cs
@@ -1,23 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PushingBoxStudios.Pathfinding.PriorityQueues
 {
-    public class FibonacciHeap<TKey, TValue> : IPriorityQueue<TKey, TValue> 
+    public class FibonacciHeap<TKey, TValue> : IPriorityQueue<TKey, TValue>
         where TKey : IComparable, IComparable<TKey>
     {
+        private LinkedList<FibonacciHeapNode<TKey, TValue>> _rootList;
         private FibonacciHeapNode<TKey, TValue> _minNode;
 
         public int Count { get; private set; }
 
         public void Clear()
         {
+            _rootList = new LinkedList<FibonacciHeapNode<TKey, TValue>>();
             _minNode = null;
             Count = 0;
         }
 
         public void DecreaseKey(IPair<TKey, TValue> node, TKey newKey)
         {
-            throw new System.NotImplementedException();
+            var hNode = node as FibonacciHeapNode<TKey, TValue>;
+
+            hNode.Key = newKey;
+
+            var parent = hNode.Parent;
+
+            do
+            {
+                if (parent.IsMarked)
+                {
+                    Cut(parent);
+                }
+                else
+                {
+                    parent.IsMarked = true;
+                    break;
+                }
+            }
+            while (parent != null);
         }
 
         public void Delete(IPair<TKey, TValue> node)
@@ -32,17 +53,48 @@ namespace PushingBoxStudios.Pathfinding.PriorityQueues
 
         public IPair<TKey, TValue> Pop()
         {
-            throw new System.NotImplementedException();
+            var node = _minNode;
+
+            return node;
         }
 
-        public IPair<TKey, TValue> Push(TKey key, TValue val)
+        public IPair<TKey, TValue> Push(TKey key, TValue value)
+        {
+            var node = new FibonacciHeapNode<TKey, TValue>(key, value);
+
+            _rootList.AddLast(node);
+            Count++;
+
+            if (node.Key.CompareTo(_minNode.Key) < 0)
+            {
+                _minNode = node;
+            }
+
+            return node;
+        }
+
+        private void Union(FibonacciHeapNode<TKey, TValue> rootNodeA, FibonacciHeapNode<TKey, TValue> rootNodeB)
         {
             throw new System.NotImplementedException();
         }
 
-        public void Union(IPriorityQueue<TKey, TValue> other)
+        private void Consolidate()
         {
-            throw new System.NotImplementedException();
+            return;
+        }
+
+        private void Cut(FibonacciHeapNode<TKey, TValue> node)
+        {
+            node.Parent = null;
+
+            var prev = node.Previous;
+            var next = node.Next;
+
+            prev.Next = next;
+            next.Previous = prev;
+
+            node.Next = null;
+            node.Previous = null;
         }
     }
 }
--- Assets/Scripts/Pathfiding/PriorityQueue/FibonacciHeapNode.cs
+++ Assets/Scripts/Pathfiding/PriorityQueue/FibonacciHeapNode.cs
@@ -1,29 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PushingBoxStudios.Pathfinding.PriorityQueues
 {
     internal class FibonacciHeapNode<TKey, TValue> : IPair<TKey, TValue> where TKey : IComparable, IComparable<TKey>
     {
-        public TKey Key { get; private set; }
+        public TKey Key { get; internal set; }
 
-        public TValue Value { get; private set; }
+        public TValue Value { get; internal set; }
 
         public int Degree { get; set; }
 
+        public bool IsMarked { get; set; }
+
         public FibonacciHeapNode<TKey, TValue> Parent { get; set; }
 
-        public FibonacciHeapNode<TKey, TValue> Pervious { get; set; }
+        public FibonacciHeapNode<TKey, TValue> Previous { get; set; }
 
         public FibonacciHeapNode<TKey, TValue> Next { get; set; }
 
-        public FibonacciHeapNode<TKey, TValue> Child { get; set; }
-
-        public bool IsMarked { get; set; }
+        public IList<FibonacciHeapNode<TKey, TValue>> Children { get; set; }
 
         public FibonacciHeapNode(TKey key, TValue value)
         {
             Key = key;
             Value = value;
+            Previous = this;
+            Next = this;
+            Children = new List<FibonacciHeapNode<TKey, TValue>>();
         }
     }
 }
